aa1b32c8d0e081553c5d8f156135f2d78c2abc47
Geekey <105234248+Geekey101@users.noreply.github.com>
2022-06-10 19:27:26 +0800

    Update notability.js

--- -X-Public-Scripts/notability.js
+++ -X-Public-Scripts/notability.js
@@ -11,7 +11,7 @@
 [rewrite_local]
 
 # Notability解锁订阅
-^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/NotabilityProCrack.js
+^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/Geekey101/-X-Public/main/-X-Public-Scripts/notability.js
 
 [mitm] 
 
