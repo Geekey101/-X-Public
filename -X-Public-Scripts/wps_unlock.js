408c688be49e14095d8272259c0ca224a710b6e5
Geekey <105234248+Geekey101@users.noreply.github.com>
2022-07-10 17:27:54 +0800

    Update wps_unlock.js

--- -X-Public-Scripts/wps_unlock.js
+++ -X-Public-Scripts/wps_unlock.js
@@ -7,7 +7,7 @@ QuantumultX:
 [rewrite_local]
 ^https?:\/\/account\.wps\.cn\/api(\/users|\/v3\/mine\/vips) url script-response-body https://raw.githubusercontent.com/Geekey101/-X-Public/main/-X-Public-Scripts/wps_unlock.js
 # > WPS解锁稻壳会员
-^https?:\/\/account\.wps\.cn\/api\/v3\/mine\/vips url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPuserCrack.js
+#^https?:\/\/account\.wps\.cn\/api\/v3\/mine\/vips url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPuserCrack.js
 ^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPowerCrack.js
 [mitm]
 hostname =  *.docer.wps.cn, vipapi.wps.cn, account.wps.cn
