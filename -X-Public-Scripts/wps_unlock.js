2433d9c8e0ab978dabe86446ff32a30fc1513e10
Geekey <105234248+Geekey101@users.noreply.github.com>
2022-07-10 17:24:26 +0800

    Update wps_unlock.js

--- -X-Public-Scripts/wps_unlock.js
+++ -X-Public-Scripts/wps_unlock.js
@@ -1,4 +1,4 @@
-/*
+jin/*
 WPS Office 解锁部分功能
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
@@ -8,18 +8,11 @@ QuantumultX:
 
 [rewrite_local]
 ^https?:\/\/account\.wps\.cn\/api(\/users|\/v3\/mine\/vips) url script-response-body https://raw.githubusercontent.com/Geekey101/-X-Public/main/-X-Public-Scripts/wps_unlock.js
-
+# > WPS解锁稻壳会员
+^https?:\/\/account\.wps\.cn\/api\/v3\/mine\/vips url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPuserCrack.js
+^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPowerCrack.js
 [mitm]
-hostname = account.wps.cn
-
-***************************
-Surge4 or Loon:
-
-[Script]
-http-response ^https?:\/\/account\.wps\.cn\/api\/users requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/Geekey101/-X-Public/main/-X-Public-Scripts/wps_unlock.js
-
-[MITM]
-hostname = account.wps.cn
+hostname =  *.docer.wps.cn, vipapi.wps.cn, account.wps.cn
 
 **************************/
 
@@ -86,6 +79,73 @@ var obj = {
 }
 
 }
+
+if($request.url. indexOf('user/v1/vip') != -1)
+{
+obj.vips = [
+    {
+      "memberid" : 12,
+      "expire_time" : 1846256142,
+      "name" : "稻壳会员",
+      "has_ad" : 0,
+      "enabled" : null
+    }
+];
+}
+if($request.url. indexOf('android/mb/buy') != -1)
+{
+obj.vips = [
+    {
+      "memberid" : 12,
+      "expire_time" : 1846256142,
+      "name" : "稻壳会员",
+      "has_ad" : 0,
+      "enabled" : null
+    }
+];
+}
+if($request.url. indexOf('download/v1/ios') != -1)
+{
+obj.vips = [
+    {
+      "memberid" : 12,
+      "expire_time" : 1846256142,
+      "name" : "稻壳会员",
+      "has_ad" : 0,
+      "enabled" : null
+    }
+];
+}
+if($request.url. indexOf('partner/invoke/usable') != -1)
+{
+obj.vips = [
+    {
+      "memberid" : 12,
+      "expire_time" : 1846256142,
+      "name" : "稻壳会员",
+      "has_ad" : 0,
+      "enabled" : null
+    }
+];
+}
+if($request.url. indexOf('api/v1/mobile/mb?/detail') != -1)
+{
+
+}
+if($request.url. indexOf('rank/v1/mobile/mb?/detail') != -1)
+{
+obj.vips = [
+    {
+      "memberid" : 12,
+      "expire_time" : 1846256142,
+      "name" : "稻壳会员",
+      "has_ad" : 0,
+      "enabled" : null
+    }
+];
+}
+
+
 
 
 $done({ body: JSON.stringify(obj) });
